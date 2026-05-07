d4116c1a7899eaabeb7a87717f0185224ce504fd
claireii64 <karenclaire04@gmail.com>
2026-05-07 13:53:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-console.log("BI Portfolio Loaded");
+const links = document.querySelectorAll(".nav-links a");
 
-/* SIMPLE SCROLL ANIMATION */
-
-const cards = document.querySelectorAll(
-  ".stats-card, .expertise-card, .project-card"
-);
-
-const observer = new IntersectionObserver((entries) => {
-
-  entries.forEach((entry) => {
-
-    if(entry.isIntersecting){
-
-      entry.target.style.opacity = "1";
-      entry.target.style.transform = "translateY(0px)";
-
-    }
-
-  });
-
-}, {
-  threshold:0.15
-});
-
-cards.forEach((card) => {
-
-  card.style.opacity = "0";
-  card.style.transform = "translateY(40px)";
-  card.style.transition = "all 0.7s ease";
-
-  observer.observe(card);
-
+links.forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
 });
